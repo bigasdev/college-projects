f0466042a3c09c4191466f354b1f8ac9eec8b320
GUSTAVO CASSIO DOMINGUES <0040962013027@americana.fatec.br>
2022-08-24 20:23:37 -0300

    almost done exercicio 7

--- web-jscript/tarefa.js
+++ web-jscript/tarefa.js
@@ -37,6 +37,29 @@ function handleCents(value1, value2){
     console.log(`Resultado: R$${r[0]},${r[2]}${r[3]}`);
 }
 
+function juroSimples(value1,value2,value3){
+    exercicioName("6 - Taxa de juros simples");
+    var j = (value1*value2*value3)/100;
+    var montante = value1+j;
+    console.log(`Valor do juros: ${j}`);
+    console.log(`Montante: ${montante}`);
+}
+function juroComposto(value1,value2,value3){
+    exercicioName("6.5 - Taxa de juros composto");
+    var div = value2/100;
+    var montante = value1*Math.pow((1+div), value3);
+    var juros = montante-value1;
+    console.log(`Valor do juros: ${juros}`);
+    console.log(`Valor do montante: ${montante}`);
+}
+function bhaskara(ax2,bx,c){
+    exercicioName("7");
+    var delta = ((Math.pow(ax2,2))-((4*bx)*c));
+    var r = Math.sqrt(delta);
+    console.log(delta);
+    console.log(r);
+}
+
 //Exercicio 1
 numberHandle(2,2);
 //Exercicio 2
@@ -47,3 +70,8 @@ baseElevada(3,3);
 dividendoHandle(8,4);
 //Exercicio 5
 handleCents(0.1,0.2);
+//Exercicio 6
+juroSimples(600, 3, 15);
+juroComposto(300, 1, 12);
+//Exercicio 7
+bhaskara(3,-5,12);
