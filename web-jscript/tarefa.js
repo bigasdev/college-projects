03be020404affcd5254a7f352f8dcd01e2a43a5d
GUSTAVO CASSIO DOMINGUES <0040962013027@americana.fatec.br>
2022-08-27 10:49:17 -0300

    ate o exercicio 13

--- web-jscript/tarefa.js
+++ web-jscript/tarefa.js
@@ -60,8 +60,65 @@ function bhaskara(ax2,bx,c){
     console.log(r);
 }
 
+function basquete(){
+   exercicioName("8");
+   
+}
+
+function sistemaNotas(){
+   exercicioName("9");
+   
+}
+
+function divisivelPor(x){
+   exercicioName("10");
+   var div = x % 3 === 0;
+   console.log(`O Numero ${x} é divisivel = ${div}`);
+}
+
+function anoBissexto(){
+   exercicioName("11");
+   
+}
+
+function fatorial(x){
+   exercicioName("12");
+   var f = x;
+   for (let i = 1; i < x; i++) {
+        f = f*(x-i);
+   }
+   console.log(`Resultado do fatorial de ${x} = ${f}`);
+}
+
+function diaUtil(x){
+   exercicioName("13");
+   var dia = "Util";
+   switch(x){
+    case 1:
+        dia = "Fim de semana";
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        break;
+    case 6:
+        break;
+    case 7:
+        dia = "Fim de semana";
+        break;
+    default:
+        dia = "Invalido";
+        break;
+   }
+   console.log(`O dia ${x} é = ${dia}`);
+}
+
 //Exercicio 1
-numberHandle(2,2);
+numberHandle(2,2); 
 //Exercicio 2
 returnTriangle([1,0,1]);
 //Exercicio 3
@@ -75,3 +132,15 @@ juroSimples(600, 3, 15);
 juroComposto(300, 1, 12);
 //Exercicio 7
 bhaskara(3,-5,12);
+//Exercicio 8
+basquete();
+//Exercicio 9
+sistemaNotas();
+//Exercicio 10
+divisivelPor(9);
+//Exercicio 11
+anoBissexto();
+//Exercicio 12
+fatorial(6);
+//Exercicio 13
+diaUtil(15);
