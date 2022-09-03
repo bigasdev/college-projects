72fddf04a52f0b783dcda31704ee54c2eaba0386
GUSTAVO CASSIO DOMINGUES <0040962013027@americana.fatec.br>
2022-09-03 10:59:07 -0300

    spawn

--- test-classes/spawn.c
+++ test-classes/spawn.c
@@ -1,24 +1,49 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
 typedef struct{
-    Entity *entity;
-    Entity *next;
-}EntityVar;
+    char name;
+    int x;
+    int y;
+    struct Entity *next;
+}Entity;
 
-typedef struct{
-    EntityVar *top;
-}Entities;  
-
-Entities spawn;
-
-void addEntity(Entity entityObj){
-    EntityVar *entity = malloc(sizeof(EntityVar));
-    entity->entity = entityObj;
-    entity->next = spawn.top;
-    spawn.top = entity;
+void addEntity(Entity **spawn, int x, int y, char name[]){
+    Entity *aux, *entity = malloc(sizeof(Entity));
+    if(entity){
+        entity->name = name;
+        entity->x = x;
+        entity->y = y;
+        entity->next = NULL;
+        
+        // Caso a fila esteja NULL a gente vai iniciar ela com a entity criada como objeto
+        if(*spawn == NULL){
+          *spawn = entity;  
+        }else{
+            // Caso contrario a gente vai utilizar como auxiliar o objeto que estava armazenado no next;
+            aux = *spawn;
+            while(aux->next){
+                aux = aux->next;
+                // A gente vai entao pegar o next do auxiliar e assignar com a entity que criamos
+                aux->next = entity;
+            }
+            
+        }
+        
+        
+    }else{
+        // Caso nao seja possivel criar o entity a gente printa um erro de alolcacao de memoria
+        printf("Erro de alocacao de memoria");
+    }
 }
 
-void startSpawn(){
-    spawn.top = NULL;
+Entity* removeEntity(Entity **spawn){
+    Entity *remover = NULL;
+    *spawn = remover->next;
+}
+
+
+int main()
+{
+
 }
